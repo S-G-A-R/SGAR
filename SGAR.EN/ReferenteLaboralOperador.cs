56e06a52445aef825df0b49d9071f9362e8a412a
Estrella <orellanaestrella66@gmail.com>
2025-02-26 21:01:26 -0600

    Correción en la tarea "Maquetar pantalla de Ver Operador"

--- SGAR.EN/ReferenteLaboralOperador.cs
+++ SGAR.EN/ReferenteLaboralOperador.cs
@@ -11,5 +11,6 @@ namespace SGAR.EN
         public int IdOperador { get; set; }
         public string Nombre { get; set; } = string.Empty;
         public string Parentesco { get; set; } = string.Empty;
+
     }
 }
