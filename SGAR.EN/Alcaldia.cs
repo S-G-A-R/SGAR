e882ecf2aeae04375b33efa3317e1da63bc2b9e1
Maynor <1925597@clases.edu.sv>
2025-02-26 20:52:55 -0600

    Se avanzó en la tarea "Maquetar pantalla de Editar Operador"

---
+++ SGAR.EN/Alcaldia.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace SGAR.EN
+{
+    public class Alcaldia
+    {
+        public int Id { get; set; }
+        public int IdZona { get; set; }
+        public string Correo { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
