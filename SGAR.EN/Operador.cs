5b051eb4b680ffa99eab658e97dec65aae4f7040
Maynor <1925597@clases.edu.sv>
2025-02-26 20:40:30 -0600

    Correciones en "Maquetar Pantalla de Crear Operador"

--- SGAR.EN/Operador.cs
+++ SGAR.EN/Operador.cs
@@ -6,16 +6,16 @@ namespace SGAR.EN
     {
         public int Id { get; set; }
         public string Nombre { get; set; } = string.Empty;
-        public string Apellido { get; set; } = string.Empty;
-        public string TelefonoPersonal { get; set; } = string.Empty;
-        public string CorreoPersonal { get; set; } = string.Empty;
+        public string? Apellido { get; set; }
+        public string? TelefonoPersonal { get; set; }
+        public string? CorreoPersonal { get; set; } 
         public string DUI { get; set; } = string.Empty;
-        public byte[] Foto { get; set; } = new byte[0];
-        public string Ayudantes { get; set; } = string.Empty;
+        public byte[]? Foto { get; set; }
+        public string? Ayudantes { get; set; }
         public string CodigoOperador { get; set; } = string.Empty;
         public string TelefonoLaboral { get; set; } = string.Empty;
         public string CorreoLaboral { get; set; } = string.Empty;
-        public int VehiculoId { get; set; }
+        public int? VehiculoId { get; set; }
         public byte[] LicenciaDoc {  get; set; } = new byte[0];
         public byte[] AntecedentesDoc { get; set; } = new byte[0];
         public byte[] SolvenciaDoc { get; set; } = new byte[0];
