2ad351290ba6f0299cce5b0367049e34b2e436b2
Maynor <1925597@clases.edu.sv>
2025-02-26 20:28:01 -0600

    Se avanzó en la tarea "Maquetar pantalla de Crear Operador"

---
+++ SGAR.EN/Operador.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace SGAR.EN
+{
+    public class Operador
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public string TelefonoPersonal { get; set; } = string.Empty;
+        public string CorreoPersonal { get; set; } = string.Empty;
+        public string DUI { get; set; } = string.Empty;
+        public byte[] Foto { get; set; } = new byte[0];
+        public string Ayudantes { get; set; } = string.Empty;
+        public string CodigoOperador { get; set; } = string.Empty;
+        public string TelefonoLaboral { get; set; } = string.Empty;
+        public string CorreoLaboral { get; set; } = string.Empty;
+        public int VehiculoId { get; set; }
+        public byte[] LicenciaDoc {  get; set; } = new byte[0];
+        public byte[] AntecedentesDoc { get; set; } = new byte[0];
+        public byte[] SolvenciaDoc { get; set; } = new byte[0];
+        public string Password { get; set; } = string.Empty;
+        public int IdAlcaldia {  get; set; }
+    }
+}
