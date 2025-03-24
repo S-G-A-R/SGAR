15b0abfc1e981b77487542f08399da784d7d42e5
Maynor <1925597@clases.edu.sv>
2025-03-24 08:19:20 -0600

    Creación del proyecto

---
+++ SGAR.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SGAR.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
